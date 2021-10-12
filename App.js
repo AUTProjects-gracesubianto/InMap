a28fbda3f3952c1c1856793bfc8b2123274aa0ef
gracesubianto <gracesubianto@gmail.com>
2021-10-12 18:03:25 +1300

    recents icons

--- App.js
+++ App.js
@@ -15,28 +15,91 @@ import { useDimensions } from '@react-native-community/hooks';
 export default function App() {
   console.log(useDimensions());
   return (
-    //<SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "#fff",
           flex: 1,
+          flexDirection: "row", //primary
+          justifyContent: "space-evenly",
+          alignItems: "center", //secondary
+          alignContent: "center",
+          flexWrap: "wrap",
         }}>
         <View style={{
           backgroundColor: "dodgerblue",
-          flex: 2,}}
+          width: 100,
+          height: 100
+        }}
           />
-      
         <View style={{
           backgroundColor: "gold",
-          flex: 1,}}
+          width: 100,
+          height: 100}}
           />
-      
         <View style={{
           backgroundColor: "tomato",
-          flex: 1,}}
+          width: 100,
+          height: 100}}
           />
       </View>
-    //</SafeAreaView>
+
+      <View
+        style={{
+          backgroundColor: "#fff",
+          flex: 1,
+          flexDirection: "row", //primary
+          justifyContent: "space-evenly",
+          alignItems: "center", //secondary
+          alignContent: "center",
+          flexWrap: "wrap",
+        }}>
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100
+        }}
+          />
+        <View style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100}}
+          />
+        <View style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100}}
+          />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "#fff",
+          flex: 1,
+          flexDirection: "row", //primary
+          justifyContent: "space-evenly",
+          alignItems: "center", //secondary
+          alignContent: "center",
+          flexWrap: "wrap",
+        }}>
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100
+        }}
+          />
+        <View style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100}}
+          />
+        <View style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100}}
+          />
+      </View>
+    </SafeAreaView>
   );
 }
 
