bad31e0f6b46c62bd07d667119cd2bc2b469959a
gracesubianto <gracesubianto@gmail.com>
2021-10-13 23:51:13 +1300

    added category buttons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,32 +2,52 @@ import React from "react";
 import {
   Button,
   Image,
-  ImageBackground,
   SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   TouchableHighlight,
   View,
+  Pressable,
 } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 function HomeScreen({ navigation }) {
-  //const pageName = "Home";
   return (
     <SafeAreaView style={styles.background}>
+      {/* {search bar section} */}
       <View style={styles.search}>
         <TextInput style={styles.searchInput} placeholder="Search here" />
       </View>
 
+      {/* {category bars section} */}
       <View style={styles.categories}>
         <Text style={styles.categoryText}>Categories</Text>
-        <Button style={styles.button} title="Museums" />
+        <View style={styles.row}>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Museums</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Hospitals</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Malls</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Airports</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Cinemas</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.text}>Universities</Text>
+          </Pressable>
+        </View>
       </View>
 
+      {/* {recents section} */}
       <View style={styles.recents}>
         <Text style={styles.categoryText}>Recents</Text>
+        {/* {first row of icons} */}
         <View style={styles.row}>
           <TouchableHighlight onPress={() => navigation.navigate("MapView")}>
             <Image
@@ -35,14 +55,12 @@ function HomeScreen({ navigation }) {
               source={require("../app/assets/building1.jpg")}
             />
           </TouchableHighlight>
-
           <TouchableHighlight onPress={() => navigation.navigate("MapView")}>
             <Image
               style={styles.buildings}
               source={require("../app/assets/building2.jpg")}
             />
           </TouchableHighlight>
-
           <TouchableHighlight onPress={() => navigation.navigate("MapView")}>
             <Image
               style={styles.buildings}
@@ -50,7 +68,7 @@ function HomeScreen({ navigation }) {
             />
           </TouchableHighlight>
         </View>
-
+        {/* {second row of icons} */}
         <View style={styles.row}>
           <TouchableHighlight onPress={() => navigation.navigate("MapView")}>
             <Image
@@ -71,7 +89,7 @@ function HomeScreen({ navigation }) {
             />
           </TouchableHighlight>
         </View>
-
+        {/* {third row of icons} */}
         <View style={styles.row}>
           <TouchableHighlight onPress={() => navigation.navigate("MapView")}>
             <Image
@@ -123,7 +141,12 @@ const styles = StyleSheet.create({
   },
   categories: {
     flex: 2,
-    backgroundColor: "red",
+    backgroundColor: "gold",
+    //flexDirection: "row",
+    //flexWrap: "wrap",
+    //alignSelf: "center",
+    justifyContent: "space-between",
+    //position: "relative",
   },
   categoryText: {
     top: "5%",
@@ -133,6 +156,7 @@ const styles = StyleSheet.create({
     bottom: 20,
     fontWeight: "bold",
     fontStyle: "italic",
+    position: "absolute",
   },
   home: {
     alignSelf: "center",
@@ -150,18 +174,31 @@ const styles = StyleSheet.create({
   buildings: {
     width: 105,
     height: 105,
-    borderRadius: 10,
-    padding: 10,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: "gold",
   },
   row: {
     top: 35,
-    right: 40,
     justifyContent: "space-evenly",
+    position: "relative",
   },
   button: {
-    height: 20,
-    width: 60,
+    height: 35,
+    width: 300,
+    top: "10%",
+    bottom: 20,
     backgroundColor: "white",
+    borderRadius: 10,
+    borderWidth: 1,
+    alignSelf: "center",
+  },
+  text: {
+    top: 6,
+    letterSpacing: 0.25,
+    color: "black",
+    textAlign: "center",
+    justifyContent: "center",
   },
 });
 
