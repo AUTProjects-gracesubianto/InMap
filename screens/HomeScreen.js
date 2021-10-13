720e45c008c43124c97af36c7b20ca60d147f294
gracesubianto <gracesubianto@gmail.com>
2021-10-13 17:49:29 +1300

    addition of recents icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ImageBackground, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 function HomeScreen(props) {
+    //const pageName = "Home";
     return (
         
     //    <ImageBackground 
@@ -12,15 +13,56 @@ function HomeScreen(props) {
     //        <View style={styles.regbutton} ></View>
     //    </ImageBackground>
     <SafeAreaView style={styles.background}>
-        <View style={styles.header}></View>
+        <View style={styles.header}>
 
+            <Text style={styles.home}>Home</Text>
+        </View>
         <View style={styles.search}>
             <TextInput style={styles.searchInput} placeholder='Search here'/>
         </View>
 
-        <View style={styles.categories}></View>
-        
-        <View style={styles.recents}></View>
+        <View style={styles.categories}>
+        <Text style={styles.categoryText}>Categories</Text>
+        </View>
+
+        <View style={styles.recents}>
+            <Text style={styles.categoryText}>Recents</Text>
+            <View style={styles.row}>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building1.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building2.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building3.jpg')}/>
+            </View>
+            
+            <View style={styles.row}>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building4.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building5.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building6.jpg')}/>
+            </View>
+            
+            <View style={styles.row}>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building7.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building8.jpg')}/>
+            <Image 
+            style={styles.buildings}
+            source={require('../app/assets/building9.jpg')}/>
+            </View>
+        </View>
     </SafeAreaView>
     
     );
@@ -37,26 +79,59 @@ const styles = StyleSheet.create({
     },
     search: {
         height: '10%',
-        backgroundColor: "blue",
+        backgroundColor: "gold",
     },
     searchInput: {
         height: 50,
         width: '90%',
         backgroundColor: "white",
         alignSelf: "center", //centred horizontally
-        top: '15%',
+        top: '20%',
         borderRadius: 10,
         borderColor: "black",
         borderWidth: 2,
+        paddingLeft: 20,
     },
     categories: {
         height: '30%',
         backgroundColor: "red",
     },
+    categoryText: {
+        top: '3%',
+        left: '5%',
+        fontSize: 25,
+        color: "black",
+        bottom: 20,
+        fontWeight: "bold",
+        fontStyle: "italic",
+    },
+    home: {
+        alignSelf: "center",
+        top: '30%',
+        fontSize: 35,
+        color: "black",
+        fontWeight: "bold",
+    },
     recents: {
         height: '50%',
-        backgroundColor: "green",
+        backgroundColor: "gold",
+        flexDirection: "row",
+        justifyContent: "space-around",
     },
+    buildings: {
+        width: 105,
+        height: 105,
+        borderRadius: 10,
+        padding: 10,
+    },
+    row: {
+        top:30,
+        right: 40,
+        justifyContent: "space-evenly",
+        //justifyContent: "center",
+        //left: 10,
+        //alignSelf: "center",
+    }
     
 });
 
